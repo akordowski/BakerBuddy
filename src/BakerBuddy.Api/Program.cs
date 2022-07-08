e1318279f1d3a17b47e2ffd5dbf432a2773796ff
Artur Kordowski <artur.kordowski@cgi.com>
2022-07-08 15:57:54 +0200

    Add api basic configuration

--- src/BakerBuddy.Api/Program.cs
+++ src/BakerBuddy.Api/Program.cs
@@ -1,8 +1,26 @@
+using Hellang.Middleware.ProblemDetails;
+using MediatR;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Build().Configuration;
+var environment = builder.Build().Environment;
+
+builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
 
 // Add services to the container.
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddMediatR(typeof(Program));
+
+builder.Services.AddProblemDetails(options =>
+{
+    options.IncludeExceptionDetails = (_, _) => environment.IsDevelopment();
+    options.MapToStatusCode<NotImplementedException>(StatusCodes.Status501NotImplemented);
+    options.MapToStatusCode<Exception>(StatusCodes.Status500InternalServerError);
+});
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,6 +32,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+// Serilog
+if (configuration.GetValue<bool>("UseSerilogRequestLogging"))
+{
+    app.UseSerilogRequestLogging();
 }
 
 app.UseHttpsRedirection();
