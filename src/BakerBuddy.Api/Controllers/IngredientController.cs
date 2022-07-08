9d3ad9d0234801bde063d098f9ddba6c2a9f4ded
Artur Kordowski <artur.kordowski@cgi.com>
2022-07-08 21:35:15 +0200

    Add recipe and ingredient api controller

---
+++ src/BakerBuddy.Api/Controllers/IngredientController.cs
@@ -0,0 +1,72 @@
+﻿using BakerBuddy.Api.Helper;
+using BakerBuddy.Domain.Commands.Ingredients;
+using BakerBuddy.Domain.Queries.Ingredients;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace BakerBuddy.Api.Controllers;
+
+[ApiController]
+[AllowAnonymous]
+[SwaggerResponse(StatusCodes.Status200OK, StatusDescriptions.Status200OK)]
+[SwaggerResponse(StatusCodes.Status500InternalServerError, StatusDescriptions.Status500InternalServerError)]
+public class IngredientController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public IngredientController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost(ApiRoutes.Ingredient.Post)]
+    [SwaggerOperation("Creates a new ingredient.")]
+    public async Task<ActionResult> CreateIngredientAsync(CancellationToken cancellationToken)
+    {
+        var command = new CreateIngredientCommand();
+        await _sender.Send(command, cancellationToken);
+
+        return NoContent();
+    }
+
+    [HttpGet(ApiRoutes.Ingredient.Get)]
+    [SwaggerOperation("Get a ingredient.")]
+    public async Task<ActionResult> GetIngredientAsync(
+        [FromRoute(Name = "id")] int ingredientId,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetIngredientQuery(ingredientId);
+        var ingredient = await _sender.Send(query, cancellationToken);
+
+        if (ingredient is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(ingredient);
+    }
+
+    [HttpPut(ApiRoutes.Ingredient.Put)]
+    [SwaggerOperation("Updates a ingredient.")]
+    public async Task<ActionResult> UpdateIngredientAsync(CancellationToken cancellationToken)
+    {
+        var command = new UpdateIngredientCommand();
+        await _sender.Send(command, cancellationToken);
+
+        return NoContent();
+    }
+
+    [HttpDelete(ApiRoutes.Ingredient.Delete)]
+    [SwaggerOperation("Deletes a ingredient.")]
+    public async Task<ActionResult> DeleteIngredientAsync(
+        [FromRoute(Name = "id")] int ingredientId,
+        CancellationToken cancellationToken)
+    {
+        var command = new DeleteIngredientCommand(ingredientId);
+        await _sender.Send(command, cancellationToken);
+
+        return NoContent();
+    }
+}
---
+++ src/BakerBuddy.Api/Controllers/RecipeController.cs
@@ -0,0 +1,86 @@
+﻿using BakerBuddy.Api.Helper;
+using BakerBuddy.Domain.Commands.Recipes;
+using BakerBuddy.Domain.Dto;
+using BakerBuddy.Domain.Queries.Ingredients;
+using BakerBuddy.Domain.Queries.Recipes;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace BakerBuddy.Api.Controllers;
+
+public class RecipeController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public RecipeController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost(ApiRoutes.Recipe.Post)]
+    [SwaggerOperation("Creates a new recipe.")]
+    public async Task<ActionResult> CreateRecipeAsync(CancellationToken cancellationToken)
+    {
+        var command = new CreateRecipeCommand();
+        await _sender.Send(command, cancellationToken);
+
+        return NoContent();
+    }
+
+    [HttpGet(ApiRoutes.Recipe.Get)]
+    [SwaggerOperation("Get a recipe.")]
+    public async Task<ActionResult<RecipeDetailDto>> GetRecipeAsync(
+        [FromRoute(Name = "id")] int recipeId,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetRecipeQuery(recipeId);
+        var recipe = await _sender.Send(query, cancellationToken);
+
+        if (recipe is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(recipe);
+    }
+
+    [HttpGet(ApiRoutes.Recipe.Ingredient.GetAll)]
+    [SwaggerOperation("Get a recipe ngredients.")]
+    public async Task<ActionResult<RecipeDetailDto>> GetRecipeIngredientsAsync(
+        [FromRoute(Name = "id")] int recipeId,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetIngredientsQuery(recipeId);
+        var ingredients = await _sender.Send(query, cancellationToken);
+
+        if (ingredients is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(ingredients);
+    }
+
+    [HttpPut(ApiRoutes.Recipe.Put)]
+    [SwaggerOperation("Updates a recipe.")]
+    public async Task<ActionResult> UpdateRecipeAsync(CancellationToken cancellationToken)
+    {
+        var command = new UpdateRecipeCommand();
+        await _sender.Send(command, cancellationToken);
+
+        return NoContent();
+    }
+
+    [HttpDelete(ApiRoutes.Recipe.Delete)]
+    [SwaggerOperation("Deletes a recipe.")]
+    public async Task<ActionResult> DeleteRecipeAsync(
+        [FromRoute(Name = "id")] int recipeId,
+        CancellationToken cancellationToken)
+    {
+        var command = new DeleteRecipeCommand(recipeId);
+        await _sender.Send(command, cancellationToken);
+
+        return NoContent();
+    }
+}
